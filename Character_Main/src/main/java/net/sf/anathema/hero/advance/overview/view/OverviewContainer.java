b96ba25445b2afe208ccdc2bf3fc6f963e3bfb1e
vestrial <sandra.sieroux@googlemail.com>
2014-06-30 11:08:03 +0200

    Moves Overview to a tab

--- Character_Main/src/main/java/net/sf/anathema/hero/advance/overview/view/OverviewContainer.java
+++ Character_Main/src/main/java/net/sf/anathema/hero/advance/overview/view/OverviewContainer.java
@@ -1,6 +1,7 @@
 package net.sf.anathema.hero.advance.overview.view;
 
 public interface OverviewContainer {
+
   CategorizedOverview addCreationOverviewView();
 
   CategorizedOverview addExperienceOverviewView();
--- Character_Main_FX/src/main/java/net/sf/anathema/fx/hero/perspective/TaskedCharacterView.java
+++ Character_Main_FX/src/main/java/net/sf/anathema/fx/hero/perspective/TaskedCharacterView.java
@@ -4,13 +4,9 @@ import javafx.scene.Node;
 import net.miginfocom.layout.AC;
 import net.miginfocom.layout.CC;
 import net.sf.anathema.character.framework.display.CharacterView;
-import net.sf.anathema.hero.display.CharacterViewSection;
 import net.sf.anathema.character.framework.display.SectionView;
 import net.sf.anathema.character.framework.display.SubViewRegistry;
-import net.sf.anathema.framework.view.util.OptionalViewBar;
-import net.sf.anathema.fx.hero.overview.DefaultCategorizedOverview;
-import net.sf.anathema.fx.hero.overview.NullOverviewContainer;
-import net.sf.anathema.hero.advance.overview.view.CategorizedOverview;
+import net.sf.anathema.hero.display.CharacterViewSection;
 import net.sf.anathema.platform.fx.NodeHolder;
 import net.sf.anathema.platform.fx.Stylesheet;
 import org.tbee.javafx.scene.layout.MigPane;
@@ -19,11 +15,7 @@ import static net.sf.anathema.lib.gui.layout.LayoutUtils.fillWithoutInsets;
 
 public class TaskedCharacterView implements CharacterView, NodeHolder {
 
-  private CategorizedOverview creationOverviewView;
-  private CategorizedOverview experienceOverviewView;
-  private CategorizedOverview overviewView = new NullOverviewContainer();
   private final TaskedCharacterPane characterPane = new TaskedCharacterPane();
-  private final OptionalViewBar optionalViewPane = new OptionalViewBar();
   private MigPane content;
   private final SubViewRegistry subViewFactory;
   private final Stylesheet[] stylesheets;
@@ -31,20 +23,6 @@ public class TaskedCharacterView implements CharacterView, NodeHolder {
   public TaskedCharacterView(SubViewRegistry viewFactory, Stylesheet... stylesheets) {
     this.subViewFactory = viewFactory;
     this.stylesheets = stylesheets;
-  }
-
-  @Override
-  public CategorizedOverview addCreationOverviewView() {
-    DefaultCategorizedOverview newView = new DefaultCategorizedOverview();
-    this.creationOverviewView = newView;
-    return newView;
-  }
-
-  @Override
-  public CategorizedOverview addExperienceOverviewView() {
-    DefaultCategorizedOverview newView = new DefaultCategorizedOverview();
-    this.experienceOverviewView = newView;
-    return newView;
   }
 
   @Override
@@ -60,16 +38,7 @@ public class TaskedCharacterView implements CharacterView, NodeHolder {
         stylesheet.applyToParent(content);
       }
       content.add(characterPane.getNode(), new CC().grow().push());
-      content.add(optionalViewPane.getNode(), new CC().alignY("top").grow().dockEast());
-      overviewView.showIn(characterPane);
     }
     return content;
-  }
-
-  @Override
-  public void toggleOverviewView(boolean experienced) {
-    this.overviewView = experienced ? experienceOverviewView : creationOverviewView;
-    optionalViewPane.setView("Overview", characterPane.getOverview());
-    overviewView.showIn(characterPane);
   }
 }
