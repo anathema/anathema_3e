bd70fe974dd24db7fb866aa98099485da5b47d84
vestrial <sandra.sieroux@googlemail.com>
2014-06-30 14:10:22 +0200

    corrects src-Folder structure for Charm_Data

--- Charm_Data/src/main/java/net/sf/anathema/charm/data/cost/HealthCost.java
+++ Charm_Data/src/main/java/net/sf/anathema/charm/data/cost/HealthCost.java
@@ -1,8 +1,5 @@
 package net.sf.anathema.charm.data.cost;
 
-import net.sf.anathema.charm.data.cost.Cost;
-import net.sf.anathema.charm.data.cost.HealthCostType;
-
 public interface HealthCost extends Cost {
 
   HealthCostType getType();
