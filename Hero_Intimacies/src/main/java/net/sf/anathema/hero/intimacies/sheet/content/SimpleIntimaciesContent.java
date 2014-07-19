8e8bddec1d25d5a2f9a20b784cb8f42d8a8d3007
Urs Reupke <ursreupke@gmail.com>
2014-07-19 14:22:00 +0200

    Prints expanded info on Intimacies on the Sheet

--- Hero_Intimacies/src/main/java/net/sf/anathema/hero/intimacies/sheet/content/SimpleIntimaciesContent.java
+++ Hero_Intimacies/src/main/java/net/sf/anathema/hero/intimacies/sheet/content/SimpleIntimaciesContent.java
@@ -1,6 +1,7 @@
 package net.sf.anathema.hero.intimacies.sheet.content;
 
 import net.sf.anathema.hero.individual.model.Hero;
+import net.sf.anathema.hero.intimacies.model.Bond;
 import net.sf.anathema.hero.intimacies.model.IntimaciesModel;
 import net.sf.anathema.hero.intimacies.model.IntimaciesModelFetcher;
 import net.sf.anathema.hero.intimacies.model.Intimacy;
@@ -10,6 +11,8 @@ import net.sf.anathema.library.resources.Resources;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static net.sf.anathema.hero.intimacies.model.Outlook.Negative;
 
 public class SimpleIntimaciesContent extends AbstractSubBoxContent implements ListSubBoxContent {
 
@@ -26,18 +29,52 @@ public class SimpleIntimaciesContent extends AbstractSubBoxContent implements Li
   }
 
   @Override
+  public boolean useNewLineForEachEntry() {
+    return true;
+  }
+
+  @Override
   public List<String> getPrintEntries() {
     List<String> printIntimacies = new ArrayList<>();
     for (Intimacy intimacy : getModel().getEntries()) {
-      String text = intimacy.getName();
+      String text = getTextForIntimacy(intimacy);
       printIntimacies.add(text);
     }
     return printIntimacies;
   }
 
-  @Override
-  public boolean useNewLineForEachEntry() {
-    return true;
+  private String getTextForIntimacy(Intimacy intimacy) {
+    String text = intimacy.getName();
+    String outlook = getOutlookString(intimacy);
+    String strength = getStrengthString(intimacy);
+    String bond = getBondString(intimacy);
+    text += " (" + outlook + strength + bond + ")";
+    return text;
+  }
+
+  private String getBondString(Intimacy intimacy) {
+    if (intimacy.getBond() == Bond.Principle) {
+      return getResources().getString("Intimacies.Sheet.Bond.Principle");
+    }
+    return getResources().getString("Intimacies.Sheet.Bond.Tie");
+  }
+
+  private String getStrengthString(Intimacy intimacy) {
+    switch (intimacy.getStrength()) {
+      case Major:
+        return getResources().getString("Intimacies.Sheet.Strength.Major");
+      case Minor:
+        return getResources().getString("Intimacies.Sheet.Strength.Minor");
+      default:
+        return getResources().getString("Intimacies.Sheet.Strength.Defining");
+    }
+  }
+
+  private String getOutlookString(Intimacy intimacy) {
+    if (intimacy.getOutlook() == Negative) {
+      return getResources().getString("Intimacies.Sheet.Outlook.Negative");
+    }
+    return getResources().getString("Intimacies.Sheet.Outlook.Positive");
   }
 
   private IntimaciesModel getModel() {
--- Hero_Intimacies/src/test/java/net/sf/anathema/hero/intimacies/sheet/content/SimpleIntimaciesContentTest.java
+++ Hero_Intimacies/src/test/java/net/sf/anathema/hero/intimacies/sheet/content/SimpleIntimaciesContentTest.java
@@ -1,16 +1,80 @@
 package net.sf.anathema.hero.intimacies.sheet.content;
 
 import net.sf.anathema.hero.dummy.DummyHero;
+import net.sf.anathema.hero.intimacies.model.Bond;
+import net.sf.anathema.hero.intimacies.model.IntimaciesModelImpl;
+import net.sf.anathema.hero.intimacies.model.Outlook;
+import net.sf.anathema.hero.intimacies.model.Strength;
 import net.sf.anathema.lib.dummy.DummyResources;
+import org.junit.Before;
 import org.junit.Test;
 
+import static net.sf.anathema.hero.intimacies.model.Bond.*;
+import static net.sf.anathema.hero.intimacies.model.Outlook.*;
+import static net.sf.anathema.hero.intimacies.model.Strength.*;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleIntimaciesContentTest {
+  private IntimaciesModelImpl intimaciesModel = new IntimaciesModelImpl();
+  private DummyResources resources = new DummyResources();
+  private SimpleIntimaciesContent content;
+
+  @Before
+  public void setUp() throws Exception {
+    DummyHero hero = new DummyHero();
+    hero.addModel(intimaciesModel);
+    content = new SimpleIntimaciesContent(hero, resources);
+  }
 
   @Test
   public void doesNotUseSingleLinePerItem() throws Exception {
-    assertThat(new SimpleIntimaciesContent(new DummyHero(), new DummyResources()).useNewLineForEachEntry(), is(true));
+    assertThat(content.useNewLineForEachEntry(), is(true));
+  }
+
+  @Test
+  public void abbreviatesPositiveDefiningTieToPlusDT() throws Exception {
+    resources.putString("Intimacies.Sheet.Outlook.Positive", "+");
+    resources.putString("Intimacies.Sheet.Strength.Defining", "D");
+    resources.putString("Intimacies.Sheet.Bond.Tie", "T");
+    createIntimacy("Intimacy", Positive, Defining, Tie);
+    assertThat(content.getPrintEntries().get(0), is("Intimacy (+DT)"));
+  }
+
+  @Test
+  public void abbreviatesNegativeToMinus() throws Exception {
+    resources.putString("Intimacies.Sheet.Outlook.Negative", "-");
+    createIntimacy("Intimacy", Negative, Defining, Tie);
+    assertThat(content.getPrintEntries().get(0), containsString("-"));
+  }
+
+  @Test
+  public void abbreviatesMajorToMa() throws Exception {
+    resources.putString("Intimacies.Sheet.Strength.Major", "Ma");
+    createIntimacy("Intimacy", Negative, Major, Tie);
+    assertThat(content.getPrintEntries().get(0), containsString("Ma"));
+  }
+
+  @Test
+  public void abbreviatesMinorToMi() throws Exception {
+    resources.putString("Intimacies.Sheet.Strength.Minor", "Mi");
+    createIntimacy("Intimacy", Negative, Minor, Tie);
+    assertThat(content.getPrintEntries().get(0), containsString("Mi"));
+  }
+
+  @Test
+  public void abbreviatesPrincipleToP() throws Exception {
+    resources.putString("Intimacies.Sheet.Bond.Principle", "P");
+    createIntimacy("Intimacy", Negative, Minor, Principle);
+    assertThat(content.getPrintEntries().get(0), containsString("P"));
+  }
+
+  private void createIntimacy(String intimacy, Outlook outlook, Strength strength, Bond bond) {
+    intimaciesModel.setCurrentName(intimacy);
+    intimaciesModel.setCurrentBond(bond);
+    intimaciesModel.setCurrentStrength(strength);
+    intimaciesModel.setCurrentOutlook(outlook);
+    intimaciesModel.commitSelection();
   }
 }
