ce0a72993198ff1e7923e2e3ba87bf643e51bf35
vestrial <sandra.sieroux@googlemail.com>
2015-02-08 21:02:38 +0100

    reduces gaps between merits

--- Library_FX/src/main/java/net/sf/anathema/library/fx/dot/SimpleDotViewPanel.java
+++ Library_FX/src/main/java/net/sf/anathema/library/fx/dot/SimpleDotViewPanel.java
@@ -6,7 +6,7 @@ import net.sf.anathema.library.fx.layout.LayoutUtils;
 import org.tbee.javafx.scene.layout.MigPane;
 
 public class SimpleDotViewPanel implements DotViewPanel {
-  private final MigPane pane = new MigPane(LayoutUtils.withoutInsets().wrapAfter(3).fillX());
+  private final MigPane pane = new MigPane(LayoutUtils.withoutInsets().wrapAfter(3).fillX().gridGap("2", "0"));
 
   @Override
   public void remove(Node node) {
