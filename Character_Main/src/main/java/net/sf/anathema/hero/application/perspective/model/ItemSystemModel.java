19c9671fb27ee1916e586d3fbbf57a1d337d6e14
Urs Reupke <ursreupke@gmail.com>
2015-02-09 19:16:50 +0100

    Removes unused method

--- Character_Main/src/main/java/net/sf/anathema/hero/application/perspective/model/ItemSystemModel.java
+++ Character_Main/src/main/java/net/sf/anathema/hero/application/perspective/model/ItemSystemModel.java
@@ -6,8 +6,6 @@ import java.util.Collection;
 
 public interface ItemSystemModel extends ItemSelectionModel {
 
-  void collectAllExistingHeroes();
-
   Collection<CharacterItemModel> getAllKnownHeroes();
 
   Item loadItem(HeroIdentifier identifier);
