2dc0eadb1d149aaa6e41304e51070270a6e9bb1b
Urs Reupke <ursreupke@gmail.com>
2015-02-15 14:46:36 +0100

    ITextViews support suggestions for autocompletion

--- Library/src/main/java/net/sf/anathema/library/text/ITextView.java
+++ Library/src/main/java/net/sf/anathema/library/text/ITextView.java
@@ -2,6 +2,8 @@ package net.sf.anathema.library.text;
 
 import net.sf.anathema.library.event.ObjectChangedListener;
 
+import java.util.Collection;
+
 public interface ITextView {
 
   void addTextChangedListener(ObjectChangedListener<String> listener);
@@ -14,4 +16,6 @@ public interface ITextView {
 
   @SuppressWarnings("UnusedDeclaration")
   void removeAllListeners();
+
+  void suggestCompletions(Collection<String> suggestions);
 }
--- Library_FX/src/main/java/net/sf/anathema/library/fx/text/FxTextView.java
+++ Library_FX/src/main/java/net/sf/anathema/library/fx/text/FxTextView.java
@@ -1,15 +1,22 @@
 package net.sf.anathema.library.fx.text;
 
+import impl.org.controlsfx.autocompletion.SuggestionProvider;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
 import net.sf.anathema.library.event.ObjectChangedListener;
 import net.sf.anathema.library.text.ITextView;
+import org.controlsfx.control.textfield.TextFields;
 import org.jmock.example.announcer.Announcer;
+
+import java.util.Collection;
+
+import static java.util.Collections.emptyList;
 
 
 public class FxTextView implements ITextView {
+
 
   public static FxTextView SingleLine(String label) {
     return new FxTextView(label, new TextField());
@@ -30,12 +37,22 @@ public class FxTextView implements ITextView {
 
   private final TextInputControl view;
   private final Announcer<ObjectChangedListener> announcer = Announcer.to(ObjectChangedListener.class);
+  private final SuggestionProvider<String> suggestionProvider = SuggestionProvider.create(emptyList());
 
   @SuppressWarnings("unchecked")
   public FxTextView(String label, TextInputControl view) {
     this.view = view;
     view.setPromptText(label);
     view.textProperty().addListener((observableValue, s, newValue) -> announcer.announce().valueChanged(newValue));
+    initAutocompletion();
+  }
+
+  private void initAutocompletion() {
+    if (!(view instanceof TextField)) {
+      return; //not supported
+    }
+    TextField field = (TextField) view;
+    TextFields.bindAutoCompletion(field, suggestionProvider);
   }
 
   @Override
@@ -56,6 +73,12 @@ public class FxTextView implements ITextView {
   @Override
   public void removeAllListeners() {
     announcer.clear();
+  }
+
+  @Override
+  public void suggestCompletions(Collection<String> suggestions) {
+    suggestionProvider.clearSuggestions();
+    suggestionProvider.addPossibleSuggestions(suggestions);
   }
 
   public Node getNode() {
