7c8c4fea97ff747f1771d1b570a6e2bc6de6590b
Sean Mattox <sean.mattox@gmail.com>
2015-02-20 11:08:25 -0800

    Removing unused classes.

--- Character_Equipment/src/main/java/net/sf/anathema/equipment/stats/modification/StatModifier.java
+++
@@ -1,6 +0,0 @@
-package net.sf.anathema.equipment.stats.modification;
-
-public interface StatModifier {
-
-  int calculate();
-}
--- Character_Equipment/src/main/java/net/sf/anathema/equipment/stats/modification/StatsModification.java
+++
@@ -1,6 +0,0 @@
-package net.sf.anathema.equipment.stats.modification;
-
-public interface StatsModification {
-
-  int getModifiedValue(int input);
-}
