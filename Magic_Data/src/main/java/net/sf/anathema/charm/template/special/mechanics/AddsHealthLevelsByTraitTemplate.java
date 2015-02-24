9bf3e7e068bdc4c225540060171c926ecf793694
Sean Mattox <sean.mattox@gmail.com>
2015-02-24 15:41:33 -0800

    Fix break

--- Magic_Data/src/main/java/net/sf/anathema/charm/template/special/mechanics/AddsHealthLevelsByTraitTemplate.java
+++ Magic_Data/src/main/java/net/sf/anathema/charm/template/special/mechanics/AddsHealthLevelsByTraitTemplate.java
@@ -3,9 +3,7 @@ package net.sf.anathema.charm.template.special.mechanics;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.sf.anathema.charm.template.special.ISpecialCharmMechanicsTemplate;
-
-public class AddsHealthLevelsByTraitTemplate implements ISpecialCharmMechanicsTemplate {
+public class AddsHealthLevelsByTraitTemplate {
 
   public String trait;
   public Map<Integer, String[]> grantedLevels = new HashMap<>();
