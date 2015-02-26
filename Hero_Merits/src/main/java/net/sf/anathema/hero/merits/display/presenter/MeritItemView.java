053bd71209919bde5984bd5068dcd00d6e6d2499
Sean Mattox <sean.mattox@gmail.com>
2015-02-26 15:35:37 -0800

    Removing unused classes.

--- Hero_Merits/src/main/java/net/sf/anathema/hero/merits/display/presenter/MeritItemView.java
+++
@@ -1,7 +0,0 @@
-package net.sf.anathema.hero.merits.display.presenter;
-
-import net.sf.anathema.library.view.OptionalTraitItemView;
-
-
-public interface MeritItemView extends OptionalTraitItemView {
-}
--- Hero_Thaumaturgy/src/main/java/net/sf/anathema/hero/thaumaturgy/display/presenter/RitualItemView.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.hero.thaumaturgy.display.presenter;
-
-import net.sf.anathema.library.fx.dot.ExtensibleDotView;
-import net.sf.anathema.library.interaction.model.Command;
-
-public interface RitualItemView extends ExtensibleDotView {
-	void addButtonListener(Command command);
-}
