4e6c8f5eac0401429dda544d9c6f37b9b03d428e
Urs Reupke <ursreupke@gmail.com>
2015-03-09 11:53:37 +0100

    Uses Java 8 Streams to aggregate sorcery initiations

--- Hero_Spells/src/main/java/net/sf/anathema/hero/spells/model/SorceryInitiationEvaluator.java
+++ Hero_Spells/src/main/java/net/sf/anathema/hero/spells/model/SorceryInitiationEvaluator.java
@@ -5,9 +5,7 @@ import net.sf.anathema.hero.individual.model.Hero;
 import net.sf.anathema.hero.spells.data.CircleType;
 import net.sf.anathema.hero.spells.template.SpellsTemplate;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
@@ -15,32 +13,20 @@ public class SorceryInitiationEvaluator {
   private final Collection<SorceryInitiation> initiations;
 
   public SorceryInitiationEvaluator(Hero hero, SpellsTemplate template, HeroEnvironment environment) {
-    this.initiations = environment.getObjectFactory().instantiateAllImplementers(SorceryInitiation.class, hero, template);
+    this.initiations = environment.getObjectFactory().instantiateAllImplementers(SorceryInitiation.class, hero,
+            template);
   }
 
   public boolean isInitiated(CircleType circle) {
-    for (SorceryInitiation initiation : initiations) {
-      if (initiation.isInitiated(circle)) {
-        return true;
-      }
-    }
-    return false;
+    return initiations.stream().anyMatch(initiation -> initiation.isInitiated(circle));
   }
 
   public boolean canInitiate() {
-    for (SorceryInitiation initiation : initiations) {
-      if (initiation.canInitiate()) {
-        return true;
-      }
-    }
-    return false;
+    return initiations.stream().anyMatch(SorceryInitiation::canInitiate);
   }
 
   public Collection<CircleType> getCirclesToInitiateInto() {
-    List<CircleType> circles = new ArrayList<>();
-    for (SorceryInitiation initiation : initiations) {
-      circles.addAll(initiation.getCirclesToInitiateInto());
-    }
-    return circles.stream().distinct().collect(toList());
+    return initiations.stream().flatMap(
+            (initiation) -> initiation.getCirclesToInitiateInto().stream()).distinct().collect(toList());
   }
 }
