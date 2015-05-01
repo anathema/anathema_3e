7d83a32c54d500bafc1e5805569ac5b6a41677ce
vestrial <sandra.sieroux@googlemail.com>
2015-05-01 15:36:20 +0200

    adds useless action data of third edition and deletes all 2nd edition content from combat box

--- Hero_Combat/src/main/java/net/sf/anathema/hero/combat/sheet/combat/content/CombatAction.java
+++ Hero_Combat/src/main/java/net/sf/anathema/hero/combat/sheet/combat/content/CombatAction.java
@@ -2,12 +2,8 @@ package net.sf.anathema.hero.combat.sheet.combat.content;
 
 public class CombatAction {
   public String name;
-  public String speed;
-  public String dv;
 
-  public CombatAction(String name, String speed, String dv) {
+  public CombatAction(String name) {
     this.name = name;
-    this.speed = speed;
-    this.dv = dv;
   }
 }
