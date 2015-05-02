71a55d22098e31788da00c4675e425b3a850915b
Urs Reupke <ursreupke@gmail.com>
2015-05-02 08:59:20 +0200

    Updates copyright lines on the sheet

--- Hero_Sheet/src/main/java/net/sf/anathema/hero/sheet/pdf/encoder/CopyrightEncoder.java
+++ Hero_Sheet/src/main/java/net/sf/anathema/hero/sheet/pdf/encoder/CopyrightEncoder.java
@@ -1,13 +1,13 @@
 package net.sf.anathema.hero.sheet.pdf.encoder;
 
+import com.itextpdf.text.Anchor;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
 import net.sf.anathema.hero.sheet.pdf.encoder.general.Bounds;
 import net.sf.anathema.hero.sheet.pdf.encoder.graphics.HorizontalAlignment;
 import net.sf.anathema.hero.sheet.pdf.encoder.graphics.SheetGraphics;
 import net.sf.anathema.hero.sheet.pdf.encoder.graphics.SimpleColumnBuilder;
 import net.sf.anathema.hero.sheet.pdf.page.PageConfiguration;
-
-import com.itextpdf.text.Anchor;
-import com.itextpdf.text.Font;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -16,7 +16,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import static net.sf.anathema.hero.sheet.pdf.encoder.graphics.HorizontalAlignment.Center;
 import static net.sf.anathema.hero.sheet.pdf.encoder.graphics.HorizontalAlignment.Left;
 import static net.sf.anathema.hero.sheet.pdf.encoder.graphics.HorizontalAlignment.Right;
 import static net.sf.anathema.hero.sheet.pdf.page.IVoidStateFormatConstants.FONT_SIZE;
@@ -24,13 +23,9 @@ import static net.sf.anathema.hero.sheet.pdf.page.PageConfiguration.Offset;
 
 public class CopyrightEncoder {
 
-  private PageConfiguration pageConfiguration;
-  private float contentHeight;
+  private final PageConfiguration pageConfiguration;
+  private final float contentHeight;
 
-
-  public CopyrightEncoder(PageConfiguration pageConfiguration) {
-    this(pageConfiguration, pageConfiguration.getContentHeight());
-  }
 
   public CopyrightEncoder(PageConfiguration pageConfiguration, float contentHeight) {
     this.pageConfiguration = pageConfiguration;
@@ -38,35 +33,27 @@ public class CopyrightEncoder {
   }
 
   public void encodeCopyright(SheetGraphics graphics) {
-    encodeVoidStateColumn(graphics);
-    encodeAnathemaColumn(graphics);
-    encodeWhiteWolfColumn(graphics);
+    encodeProgramCopyright(graphics);
+    encodeGameCopyright(graphics);
   }
 
-  private void encodeVoidStateColumn(SheetGraphics graphics) {
+  private void encodeProgramCopyright(SheetGraphics graphics) {
     Bounds bounds = pageConfiguration.getColumnRectangle(contentHeight, getCopyrightHeight(), 1, Offset(0));
-    String text = "Inspired by Voidstate\nhttp://www.voidstate.com";
-    Anchor phrase = createAnchor(graphics, text, "http://www.voidstate.com");
+    String text = induceYear("Created with Anathema \u00A92007-{0}\nhttp://anathema.github.io");
+    Anchor phrase = createAnchor(graphics, text, "http://anathema.github.io");
     encoderLine(graphics, phrase, Left, bounds);
   }
 
-  private void encodeAnathemaColumn(SheetGraphics graphics) {
-    Bounds bounds = pageConfiguration.getColumnRectangle(contentHeight, getCopyrightHeight(), 1, Offset(1));
-    String text = induceYear("Created with Anathema \u00A92007-{0}\nhttp://anathema.sf.net");
-    Anchor phrase = createAnchor(graphics, text, "http://anathema.sf.net");
-    encoderLine(graphics, phrase, Center, bounds);
-  }
-
-  private void encodeWhiteWolfColumn(SheetGraphics graphics) {
+  private void encodeGameCopyright(SheetGraphics graphics) {
     Bounds bounds = pageConfiguration.getColumnRectangle(contentHeight, getCopyrightHeight(), 1, Offset(2));
-    String text = induceYear("Exalted \u00A92007-{0} by CCP hf\nhttp://www.white-wolf.com");
-    Anchor phrase = createAnchor(graphics, text, "http://www.white-wolf.com");
+    String text = induceYear("Exalted \u00A92007-{0} by CCP hf\nhttp://www.theonyxpath.com");
+    Anchor phrase = createAnchor(graphics, text, "http://www.theonyxpath.com");
     encoderLine(graphics, phrase, Right, bounds);
   }
 
-  private String induceYear(String pattern)                               {
+  private String induceYear(String pattern) {
     int year = new GregorianCalendar().get(Calendar.YEAR);
-    DecimalFormat format = new DecimalFormat("####",new DecimalFormatSymbols(Locale.ENGLISH));
+    DecimalFormat format = new DecimalFormat("####", new DecimalFormatSymbols(Locale.ENGLISH));
     return MessageFormat.format(pattern, format.format(year));
   }
 
@@ -84,7 +71,7 @@ public class CopyrightEncoder {
     return pageConfiguration.getPageHeight() - pageConfiguration.getContentHeight();
   }
 
-  private void encoderLine(SheetGraphics graphics, Anchor phrase, HorizontalAlignment alignment, Bounds bounds) {
+  private void encoderLine(SheetGraphics graphics, Phrase phrase, HorizontalAlignment alignment, Bounds bounds) {
     SimpleColumnBuilder column = graphics.createSimpleColumn(bounds);
     column.withLeading(FONT_SIZE).andAlignment(alignment).andTextPart(phrase).encode();
   }
